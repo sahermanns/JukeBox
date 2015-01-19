4d7ce4ffaa46c64400983e7b97a41386e8fe4e78
Brandon Burns <brandon10688@gmail.com>
2015-01-19 14:41:50 -0800

    added functionality to drop downs

--- currentProject/jukebox.js
+++ currentProject/jukebox.js
@@ -1,37 +1,30 @@
 // Jukebox is made up of Artists, as new artsists are constructed they are pushed into the array artists//
- function Jukebox (){
-  this.artists =[];
-  this.addArtist = function (artist) {
-    this.artists.push(artist);
-  };
- };
- // Artist is made up of Songs, as new songs are constructed they are pushed in the array songs//
- function Artist (){
-  this.name = ""
-  this.songs =[];
-  this.addSong = function (song) {
-  this.songs.push(song);
-  };  
- };
- // Song is made up of trackTitle and artistTitle//
- function Song (trackTitle, albumTitle){
-  this.trackTitle = trackTitle;
-  this.albumTitle = albumTitle;
- };
+function Jukebox (){
+this.artists =[];
+this.addArtist = function (artist) {
+  this.artists.push(artist);
+};
+};
+// Artist is made up of Songs, as new songs are constructed they are pushed in the array songs//
+function Artist (){
+this.name = ""
+this.songs =[];
+this.addSong = function (song) {
+this.songs.push(song);
+};  
+};
+// Song is made up of trackTitle and artistTitle//
+function Song (trackTitle, albumTitle){
+this.trackTitle = trackTitle;
+this.albumTitle = albumTitle;
+};
 
- function randomSong () {
-  var artist = sarahJams.artists[Math.floor(Math.random() * sarahJams.artists.length)];
-  var index = artist.songs[Math.floor(Math.random() * artist.songs.length)];
-  return index.trackTitle + " " + index.albumTitle;
- }
+function randomSong () {
+var artist = sarahJams.artists[Math.floor(Math.random() * sarahJams.artists.length)];
+var index = artist.songs[Math.floor(Math.random() * artist.songs.length)];
+return index.trackTitle + " " + index.albumTitle;
+}
 
-  function showSongs() {
-    document.getElementById("songs").style.display = "none";
-    document.getElementById("artists").onchange = function() {
-      document.getElementById("songs").style.display = "block";
-    }
-  } 
-} 
 var sarahJams = new Jukebox ("Sarah's Jams");
 
 var theShins = new Artist ("The Shins");
@@ -86,4 +79,28 @@ sarahJams.addArtist(beirut);
 sarahJams.addArtist(coldWarKids);
 sarahJams.addArtist(fatherJohnMisty);
 
-theShins.addSong(newSlang);
+function showSongList () {
+  document.getElementById("artists").onchange = function() {
+    document.getElementById("songs").style.display = "block";
+
+    for (var i = 0; i < document.getElementById("songs").length; i++) {
+      document.getElementById("songs")[i].style.display = "none";
+    }
+
+    var artist = document.getElementById("artists").value;
+
+    var songs = document.getElementsByClassName(artist);
+
+    for (var i = 0; i < songs.length; i++) {
+      songs[i].style.display = "block";
+    }
+  }
+} 
+
+var choose = document.getElementById("artists");
+
+choose.addEventListener('click', showSongList);
+
+
+
+
